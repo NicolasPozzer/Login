58d35ba6f94d1e1155e986aa9d1d33b4c7153204
NicolasPozzer <pozzernico@gmail.com>
2023-09-06 10:53:09 -0300

    login-funcionando

--- target/generated-sources/annotations/com/mycompany/login/logica/Usuario_.java
+++ target/generated-sources/annotations/com/mycompany/login/logica/Usuario_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-08-30T19:16:02", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-06T10:45:35", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
